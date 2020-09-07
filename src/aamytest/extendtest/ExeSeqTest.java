f06882e8da742a9dd4f8dbb43353cd869872533a
xiaominglei001 <xiaominglei001@qq.com>
2020-09-07 14:44:02 +0800

    init

---
+++ src/aamytest/extendtest/ExeSeqTest.java
@@ -0,0 +1,48 @@
+package aamytest.extendtest;
+
+/**
+ * 函数执行顺序测试
+ * Created by 萌小Q on 2017/5/17 0017.
+ */
+public class ExeSeqTest {
+    public static void main(String[] args) {
+        System.out.println(new B().getValue());//
+    }
+
+    static class A {
+        protected int value;
+
+        public A(int v) {
+            setValue(v);
+        }
+
+        public void setValue(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            try {
+                value++;
+                return value;
+            } catch (Exception e) {
+                System.out.println(e.toString());
+            } finally {
+                this.setValue(value);
+                System.out.println(value);//
+            }
+            return value;
+        }
+    }
+
+    static class B extends A {
+        public B() {
+            super(5);//这一句执行后value值为10
+            setValue(getValue() - 3);//getValue()执行时把value设置为22，并输出22，最后返回11，相当于setValue(8);
+        }
+
+        @Override
+        public void setValue(int value) {
+            super.setValue(2 * value);
+        }
+    }
+}
---
+++ src/aamytest/initialization/TestC.java
@@ -0,0 +1,8 @@
+package aamytest.initialization;
+
+
+public class TestC {
+    private TestA mA = new TestA("成员变量");
+    public static void main(String[] args) {
+    }
+}
